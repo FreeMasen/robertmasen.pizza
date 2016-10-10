d12cc50bfdea635864028d0b420a15718bd9c475
FreeMasen <RFM@Herman.local>
2016-10-09 20:20:59 -0400

    removed nav-images for text. working sub-menu

--- www/app/navigation/navigation.controller.js
+++ www/app/navigation/navigation.controller.js
@@ -1,18 +1,39 @@
 angular.module('navigation')
-    .controller('navController', ['$scope','$rootScope', '$location', function($scope, $rootScope, $location){
-        var home = {name: 'home', href:'#!/',src:'img/home-text.png'},
-            work = {name: 'work', href: '#!/work/resume',src: 'img/work-text.png'}
-            resume = {name: 'resume', href:'#!/work/resume', src:'img/resume-text.png'}
-            portfolio = {name: 'portfolio', href:'#!/work/portfolio', src:'img/portfolio-text.png'}
-            about = {name: 'about', href: '#!/about', src: 'img/about-text.png'},
-            contact = {name: 'contact', href: '#!/contact',src: 'img/contact-text.png'},
-            empty = {name: 'empty'}
-        $scope.endpoint = [];
+    .controller('navController', ['$scope','$rootScope', '$location', 
+                function($scope, $rootScope, $location) {
+        let home = {name: 'home', 
+        link: {href:"/",
+                display:'Home'}
+        },
+            work = {name: 'work', 
+            link: {href:'#!/work/resume',
+                display:'Work'},
+            subs: [
+                {name: 'resume',
+                href:'#!/work/resume',
+                display: 'Resume'},
+                {name: 'portfolio',
+                href:'#!/work/portfolio',
+                display: 'Portfolio'}
+            ]},
+            about = {name: 'about', 
+            link: {href:'#!/about',
+                display: 'About'}
+            },
+            contact = {name: 'contact', 
+            link: {href: '#!/contact',
+                display: 'Contact'}
+            }        
+            $scope.endpoint = [];
         $scope.isSub = false
         $scope.elements = [home, work, about, contact]
         $rootScope.$on('$locationChangeSuccess', function() {
                 updateNav();
         });
+        $scope.highlighted = ''
+        $scope.highlight = function(name) {
+            $scope.highlighted = name
+        }
 
         function updateNav() {
             $scope.endpoint = $location.path().split('/').splice(1);
@@ -21,7 +42,6 @@ angular.module('navigation')
                 $scope.endpoint = ['home']
             }
             if ($scope.endpoint.includes('work')) {
-                $scope.subNav = [resume, portfolio];
                 $scope.isSub = true
             } else {
                 $scope.isSub = false;
