6e9eeee8c56ec436e44464db61802ff602f478f2
FreeMasen <RFM@Herman.local>
2016-10-10 23:07:19 -0400

    add filter/map of response for repos and events

--- src/github.js
+++ src/github.js
@@ -6,12 +6,6 @@ var db = mongo('rm', ['tokens']);
 
 function getToken(cb) {
     db.tokens.findOne({name: "gh"}, (err, doc) => {
-        cb(err, doc);
-    })
-}
-
-function requestRepoInfo(cb) {
-    getToken((err, doc) => {
         if (err) { 
             cb(err);
             return;
@@ -20,22 +14,97 @@ function requestRepoInfo(cb) {
             cb(new Error('Cound not find gh token'));
             return;
         }
-        request.get('https://api.github.com/users/freemasen/events', {
-            auth: {
-                user: doc.token
-            },
-            headers: {
-                "User-Agent": "freemasen"
-            }
-        }, (err, res, body) => {
+        cb(null, doc)
+    })
+}
+
+function requestRepoInfo(cb) {
+    getToken((err, doc) => {
+        if (err){
+             cb(err)
+             return
+        }
+        let opts = composeRequestOptions(doc.token)
+        request.get('https://api.github.com/users/freemasen/repos',opts
+        , (err, res, body) => {
             if (err) {
                 cb(err);
                 return
             }
-            fs.writeFile(process.cwd() + '/response.txt', body ,(e) => {})
-            cb(undefined, res, body)
+            
+            cb(null, mapRepos(filterRepos(body)))  
         })
     })
 }
 
-module.exports = requestRepoInfo;
+function requestEvents(cb) {
+    getToken((err, doc) => {
+        if (err){
+            cb(err)
+            return
+        }
+        let opts = composeRequestOptions(doc.token)
+        request('https://api.github.com/users/freemasen/events', opts, (err, res, body) => {
+            if (err) {
+                cb(err)
+                return
+            }
+            cb(null, mapEvents(filterEvents(body)))
+        })
+    })
+}
+
+function composeRequestOptions(token) {
+    return {
+        auth: {
+            user: token
+        },
+        headers: {
+            'User-Agent': 'freemasen'
+        }
+    }
+} 
+
+function filterRepos(repos) {
+    let parsed = JSON.parse(repos)
+    return parsed.filter((repo) => {
+        return repo.homepage != null &&
+            repo.homepage != ''
+
+    })
+}
+
+function mapRepos(repos) {
+    return repos.map((repo) => {
+        return {
+            name: repo.name,
+            url: repo.html_url,
+            mine: repo.owner.login == 'FreeMasen',
+            stars: repo.stargazers_count,
+            language: repo.language
+        }
+    })
+}
+
+function filterEvents(events) {
+    return events.splice(0, 10)
+}
+
+function mapEvents(events) {
+    return events.map((event) => {
+        return {
+            time: event.created_at,
+            repo: {
+                name: event.repo.name.split('/')[1],
+                url: event.repo.url
+            },
+            message: event.commits[0].message,
+            
+        }
+    })
+}
+
+module.exports = {
+    repos: requestRepoInfo,
+    events: requestEvents
+}
