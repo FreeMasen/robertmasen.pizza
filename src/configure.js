c0998b8b0ef2e65d979e2838b89bdc4c4f309999
FreeMasen <RFM@Herman.local>
2016-10-12 19:35:46 -0400

    increased compression

--- src/configure.js
+++ src/configure.js
@@ -6,7 +6,7 @@ const morgan = require('morgan')
 app.use(morgan('dev'))
 
 const compression = require('compression')
-app.use(compression())
+app.use(compression({level: 9}))
 
 const bodyparser = require('body-parser')
 app.use(bodyparser.json())
