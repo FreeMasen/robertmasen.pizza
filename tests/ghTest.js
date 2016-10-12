5594938c83290ca77a1df3991406962e41eb9d06
FreeMasen <RFM@Herman.local>
2016-10-12 11:37:12 -0400

    Finalized GitHub test

--- tests/ghTest.js
+++ tests/ghTest.js
@@ -3,15 +3,43 @@ var gh = require('../src/github.js');
 
 describe('Github', function() {
     describe('request repo info', function() {
-        it('should not have an error', function(done) {
-            gh((err, res, body) => {
+        it('callback should have err == null and repos != undefined', function(done) {
+            gh.repos((err, repos) => {
                 if (err) {
                     done(err);
                 }
-                assert(res != undefined, 'response was undefined');
-                assert(body != undefined, 'body was undefined');
+                it('repos != undefined', function(done) {
+                    assert(repos != undefined, 'repos was undefined')
+                    done()
+                })
+                it('typeof repos == "object', function(done) {
+                    assert(typeof repos == 'object', 'repos was not an object')
+                })
+                it('Array.isArray(repos)', function(done) {
+                    assert(Array.isArray(repos), 'Repos was not an array')
+                })
                 done();
-            });
-        });
-    });
-});
+            })
+        })
+    })
+    describe('request events info', function() {
+        it('callback should have err == nul and res.body != undefined', function(done) {
+            gh.events((err, events) => {
+                if(err) {
+                    done(err)
+                }
+                it('events != undefined', function(done) {
+                    assert(events != undefined, 'events was undefined')
+                    done()
+                })
+                it('typeof events == "object', function(done) {
+                    assert(typeof events == 'object', 'events was not an object')
+                })
+                it('Array.isArray(events)', function(done) {
+                    assert(Array.isArray(events), 'events was not an array')
+                })
+                done();
+            })  
+        })
+    })
+})
