8b0bfe0a904190316fb62bceb1bff9f719563805
FreeMasen <RFM@Herman.local>
2016-12-13 13:12:22 -0600

    add postfix exec for testing

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,15 @@
 const mongo = require('mongojs')
 const db = mongo('rm', ['contact'])
+const {exec} = require('child_process')
 
 function addContact(obj, cb) {
+    let mailBody = `name: ${obj.name}
+                    reason: ${obj.reason}
+                    company: ${obj.company || "N/A"}
+                    roll: ${obj.roll | "N/A"}
+                    content: ${obj.content}`
+    let command = `echo ${mailBody} | mail -s "rm.p contact form r.f.masen@gmail.com`
+    exec(command)
     db.contact.insert(obj, (err) => {
         if (err) {
             cb(err)
