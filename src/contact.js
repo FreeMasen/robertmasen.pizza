33186a888cc651bb269706f09c17dcaa488e1b24
FreeMasen <RFM@Herman.local>
2016-12-14 12:18:07 -0600

    added quotes

--- src/contact.js
+++ src/contact.js
@@ -3,8 +3,8 @@ const db = mongo('rm', ['contact'])
 const {exec} = require('child_process')
 
 function addContact(obj, cb) {
-    let mailBody = `<html><head></head><body><span>name: ${obj.name}</span><br/><span>reason: ${obj.reason}<span><span>company: ${obj.company || "N/A"}</span><br/><span>roll: ${obj.roll | "N/A"}</span><br/><span>content: ${obj.content}</span><br/></body></html>`
-    let command = `echo ${mailBody} | mail -s "rm.p contact form r.f.masen@gmail.com`
+    let mailBody = `"<html><head></head><body><span>name: ${obj.name}</span><br/><span>reason: ${obj.reason}<span><span>company: ${obj.company || "N/A"}</span><br/><span>roll: ${obj.roll | "N/A"}</span><br/><span>content: ${obj.content}</span><br/></body></html>`
+    let command = `echo ${mailBody} | mail -s "rm.p contact form r.f.masen@gmail.com"`
     exec(command, (err, stdOut, stdErr) => {
         if (err) console.log(err.message)
         if (stdOut) console.log(stdOut)
