36bfd7dbbb23626a8c859bcac69a3107cfc6f99f
Robert Masen <rmasen@USIHQ.net>
2018-03-02 12:59:22 -0600

    add font ext

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,22 @@ fn main() {
     let cwd_str = cwd.to_string_lossy();
     println!("starting from {:?}", cwd_str);
     pb.use_static(&(cwd_str + "/../../www/"));
+    pb.set_know_extensions(&["html",
+            "js",
+            "css",
+            "ico",
+            "jpg",
+            "png",
+            "woff2",
+            "ttf",
+            "txt",
+            "xml",
+            "rss",
+            "svg",
+            "txt",
+            "gif",
+            "map",
+            "otf"]);
     let h = Http::new().bind(&addr, move || pb.new_service()).expect("Unable to start server on 4444");
     println!("Listening on 4444");
     let _ = h.run().expect("Unable to start server on 4444");
