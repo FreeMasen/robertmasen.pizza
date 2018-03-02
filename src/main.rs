d517248d05a892b263775b664448ad991bf4c474
Robert Masen <rmasen@USIHQ.net>
2018-03-02 10:14:41 -0600

    add rust server

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate hyper;
+extern crate pony;
+use std::env;
+use hyper::server::{NewService,Http};
+use pony::pony_builder::{PonyBuilder};
+
+fn main() {
+    let addr = "127.0.0.1:4444".parse().unwrap();
+    let mut pb = PonyBuilder::new();
+    let mut cwd = env::current_exe().expect("unable to get current exe");
+    cwd.pop();
+    let cwd_str = cwd.to_string_lossy();
+    println!("starting from {:?}", cwd_str);
+    pb.use_static(&(cwd_str + "/../../www/"));
+    let h = Http::new().bind(&addr, move || pb.new_service()).expect("Unable to start server on 4444");
+    println!("Listening on 4444");
+    let _ = h.run().expect("Unable to start server on 4444");
+    println!("Exiting server");
+}
